e18e501d2204aa113772b442d17f371781880413
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2022-10-11 00:52:59 -0300

    Fix height problems

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ const App = () => {
 
   return (
     <div className="grid place-items-center min-h-screen bg-gradient-to-r from-[#FCD04C] to-[#F97416] p-4">
-      <div className="p-4 max-w-5xl grid gap-4 sm:grid-cols-5 w-full">
-        <div className="sm:col-span-3 w-full">
+      <div className="p-4 max-w-5xl grid gap-4 sm:grid-cols-5 w-full h-full">
+        <div className="sm:col-span-3 w-full self-center">
           <Stopwatch />
         </div>
-        <div className="sm:col-span-2 sm:border-l p-4 space-y-4 w-full">
+        <div className="sm:col-span-2 sm:border-l p-4 space-y-4 w-full h-full flex flex-col justify-center">
           <AddMemberInput members={members} setMembers={setMembers} />
           <Team members={members} />
         </div>
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,20 +1,22 @@
 const Team = ({ members }) => {
   return (
-    <div className="text-white">
+    <div className="text-white md:h-[284px] h-72">
       <h3 className="text-xl">Team</h3>
-      {members.length > 0 ? (
-        <ul className="mt-2">
-          {members.map((member, idx) => {
-            return (
-              <li key={idx}>
-                {idx + 1}. {member}
-              </li>
-            )
-          })}
-        </ul>
-      ) : (
-        <p className="">You haven{"'"}t added any member yet :(</p>
-      )}
+      <div className="h-64 overflow-auto">
+        {members.length > 0 ? (
+          <ul className="mt-2">
+            {members.map((member, idx) => {
+              return (
+                <li key={idx}>
+                  {idx + 1}. {member}
+                </li>
+              )
+            })}
+          </ul>
+        ) : (
+          <p className="mt-2">You haven{"'"}t added any member yet :(</p>
+        )}
+      </div>
     </div>
   )
 }
