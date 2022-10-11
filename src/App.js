1a906b1a1f1ff6bef9c1d08be21e02be2184dee6
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2022-10-11 08:07:49 -0300

    Use local storage to persist team members

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import { AddMemberInput, Stopwatch, Team } from "./components"
 
 const App = () => {
-  const [members, setMembers] = useState([])
+  const [members, setMembers] = useState(() => {
+    const membersSaved = localStorage.getItem("members")
+    const initialValue = JSON.parse(membersSaved)
+    return initialValue || []
+  })
+
+  useEffect(() => {
+    localStorage.setItem("members", JSON.stringify(members))
+  }, [members])
 
   return (
     <div className="flex flex-col justify-between min-h-screen bg-gradient-to-r from-[#FCD04C] to-[#F97416]">
       <nav className="p-4">
-        <h1 className="text-4xl text-white text-center sm:text-left">Time Your Team</h1>
+        <h1 className="text-4xl text-white text-center sm:text-left">
+          Time Your Team
+        </h1>
       </nav>
       <div className="grid place-items-center p-4">
         <div className="p-4 max-w-5xl grid gap-4 sm:grid-cols-5 w-full h-full">
           <div className="sm:col-span-3 w-full self-center">
-            <Stopwatch members={members} setMembers={setMembers}/>
+            <Stopwatch members={members} setMembers={setMembers} />
           </div>
           <div className="sm:col-span-2 sm:border-l p-4 space-y-4 w-full h-full flex flex-col justify-center">
             <AddMemberInput members={members} setMembers={setMembers} />
@@ -22,7 +32,14 @@ const App = () => {
         </div>
       </div>
       <footer className="p-4 text-center text-white text-sm">
-        Made with ❤️ by <a className="hover:text-slate-200" href="https://jorgealvarado.me/" target="_blank">Jorge Alvarado</a>
+        Made with ❤️ by{" "}
+        <a
+          className="hover:text-slate-200"
+          href="https://jorgealvarado.me/"
+          target="_blank"
+        >
+          Jorge Alvarado
+        </a>
       </footer>
     </div>
   )
